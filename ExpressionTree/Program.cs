bfeaee6f4bf5564079f093b06c0e08cb2035a99f
shoaibalirts <shoaibali.rts@gmail.com>
2025-04-20 12:35:08 +0200

    created student class, and assign its properties

--- ExpressionTree/Program.cs
+++ ExpressionTree/Program.cs
@@ -3,17 +3,24 @@ using System.Linq.Expressions;
 
 namespace ExpressionTree
 {
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
     class Program
     {
         static void Main()
         {
+            Student student = new Student() { StudentID = 101, StudentName = "Shoaib Ali", Age = 42 };
             // Create expression tree with Func
             Expression<Func<int, int>> expression = a => a * a;//receiving parameter a and return square value
 
             // Compile expression using Compile method to invoke it as Delegate
             Func<int, int> myDelegate = expression.Compile(); // myDelegate contains the reference of the lamda expression as mentioned above
 
-            // Execute the mthod
+            // Execute the method
             Console.WriteLine(myDelegate.Invoke(10));
         }
     }
