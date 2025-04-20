f22a5e159d0533091106213a4a49bcdc9ed50702
shoaibalirts <shoaibali.rts@gmail.com>
2025-04-20 12:30:19 +0200

    create expression, compile expression, and execute method

--- ExpressionTree/Program.cs
+++ ExpressionTree/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace ExpressionTree
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExpressionTree
 {
     class Program
     {
         static void Main()
         {
+            // Create expression tree with Func
+            Expression<Func<int, int>> expression = a => a * a;//receiving parameter a and return square value
+
+            // Compile expression using Compile method to invoke it as Delegate
+            Func<int, int> myDelegate = expression.Compile(); // myDelegate contains the reference of the lamda expression as mentioned above
+
+            // Execute the mthod
+            Console.WriteLine(myDelegate.Invoke(10));
         }
     }
 }
